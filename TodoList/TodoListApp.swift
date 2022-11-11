1753445c8d326827d4ea06345bc1e461502793bc
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2022-11-11 22:18:11 +0100

    A new project!
    In this commit, a new Xcode project was simply created for an iOS app.

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nunzio Ricci on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
