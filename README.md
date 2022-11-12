08b76c5dfed8a1b3735aaae55b9290b7b51a1fb1
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2022-11-12 04:34:00 +0100

    Interface creation
    In this commit, the interface that can be seen on Figma has been reproduced. The code is commented in such a way that it is always possible to see which element corresponds to each block. Now all the code is hard coded to perfectly mimic the design on Figma.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # swiftui-tutorial
 This repository is designed to be used as a SwiftUI tutorial. Each commit is like a small chapter.
+
+[Here](https://www.figma.com/file/CCCsROzaY3CI8FjKwMtjmn/Untitled?node-id=0%3A1) you can find screens of the application we want to create.
--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -8,14 +8,193 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showModal: Bool = false
+    @State var newTask: String = "Buy a new keyboa"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        // Main page
+        ScrollView {
+            VStack {
+                // Header
+                HStack {
+                    // Title
+                    Text("Todo List")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    // Open modal button
+                    Button {
+                        showModal = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.largeTitle)
+                    }
+                    
+                } // End HStack
+                .padding()
+                
+                Spacer(minLength: 15)
+                
+                // Body
+                VStack {
+                    
+                    // First task
+                    HStack {
+                        // Checkmark
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                        
+                        Spacer()
+                        
+                        // Task text
+                        Text("Coding for an hour")
+                            .foregroundColor(.secondary)
+                            .strikethrough()
+                        
+                        Spacer()
+                        
+                        // Delete task button
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                        
+                    } // End HStack
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 18)
+                    
+                    // Second task
+                    HStack {
+                        // Checkmark
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                        
+                        Spacer()
+                        
+                        // Task text
+                        Text("Teach to a monkey to write code")
+                            .foregroundColor(.secondary)
+                            .strikethrough()
+                        
+                        Spacer()
+                        
+                        // Delete task button
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                        
+                    } // End HStack
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 18)
+                    
+                    // Third task
+                    HStack {
+                        // Square
+                        Image(systemName: "square")
+                        
+                        Spacer()
+                        
+                        // Task text
+                        Text("Just another task")
+                        
+                        Spacer()
+                        
+                        // Delete task button
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                        
+                    } // Fourth HStack
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 18)
+                    
+                    // Fourth task
+                    HStack {
+                        // Square
+                        Image(systemName: "square")
+                        
+                        Spacer()
+                        
+                        // Task text
+                        Text("Write the next algorithm")
+                        
+                        Spacer()
+                        
+                        // Delete task button
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                        
+                    } // End HStack
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 18)
+                    
+                    // Fifth task
+                    HStack {
+                        // Checkmark
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                        
+                        Spacer()
+                        
+                        // Task text
+                        Text("Support other monkeys")
+                            .foregroundColor(.secondary)
+                            .strikethrough()
+                        
+                        Spacer()
+                        
+                        // Delete task button
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                        
+                    } // End HStack
+                    .padding(.horizontal)
+                    
+                    Spacer(minLength: 18)
+                    
+                } // End VStack
+            } // End VStack
+        }// End ScrollView
+        
+        // Modal
+        .sheet(isPresented: $showModal) {
+            Spacer(minLength: 30)
+            VStack {
+                HStack {
+                    // Title
+                    Text("Add Task")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                } // End HStack
+                .padding()
+                
+                TextField("Your task here", text: $newTask)
+                    .padding()
+                
+                // Add button on the left
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        // future code here
+                    } label: {
+                        Text("Add")
+                            .fontWeight(.semibold)
+                            .padding(6)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                } // End HStack
+                .padding()
+                
+                Spacer()
+            } // End VStack
+        } // End modal
+
     }
 }
 
